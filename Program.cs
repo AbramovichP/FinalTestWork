5a44a0c01c7353f16dc705949b0bcc43166f9993
Petr_Abramovich <malyarpetr@gmail.com>
2022-08-05 23:25:55 +0300

    добавлен код создания массива и пока пустой файл блок-схемы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Метод создает массив из введеных пользователем эллементов из консоли
+// elements - кол-во элементов массива
+string[] CreateArray(int elements)
+{
+    string[] array = new string[elements];
+
+    for(int i = 0; i < elements; i++)
+    {
+        Console.WriteLine($"Введите {i} эллемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] myarray = CreateArray(n);
+Console.WriteLine("[" + string.Join(" ", myarray) + "]");
+
