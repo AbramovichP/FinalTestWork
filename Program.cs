ddbca1548c3e39bfe438a8e6837e9867c529796a
Petr_Abramovich <malyarpetr@gmail.com>
2022-08-06 13:01:46 +0300

    написан работающий код

--- Program.cs
+++ Program.cs
@@ -1,20 +1,33 @@
-﻿// Метод создает массив из введеных пользователем эллементов из консоли
-// elements - кол-во элементов массива
-string[] CreateArray(int elements)
+﻿// Метод создает массив из введеных пользователем эллементов с клавиатуры
+// elements - кол-во элементов массива(задается пользователем с клавиатуры)
+string[] CreateArrayWords(int elements)
 {
-    string[] array = new string[elements];
+    string[] words = new string[elements];
 
     for(int i = 0; i < elements; i++)
     {
-        Console.WriteLine($"Введите {i} эллемент массива: ");
-        array[i] = Console.ReadLine();
+        Console.WriteLine($"Введите {i+1} эллемент массива: ");
+        string? n = Console.ReadLine();
+        if(n != null)
+            words[i] = n;
     }
-    return array;
+    return words;
 }
 
+string[] FormingArrayStrings(string[] words)
+{
+    string[] wordArray = new string[words.Length];
+    for(int i = 0; i < words.Length; i++)
+    {
+        if(words[i].Length <= 3)wordArray[i] = words[i];
+    }
+    return wordArray;
+}
 Console.WriteLine("Введите кол-во элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-string[] myarray = CreateArray(n);
-Console.WriteLine("[" + string.Join(" ", myarray) + "]");
+string[] mywords = CreateArrayWords(n);
+Console.WriteLine("[" + string.Join(" ", mywords) + "]");
 
+string[] collectedWord = FormingArrayStrings(mywords);
+Console.WriteLine("[" + string.Join(" ",collectedWord) + "]");
