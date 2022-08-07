3b9894880cc8bc5cc3e30facaaf9ed6a24fef8e2
Petr_Abramovich <malyarpetr@gmail.com>
2022-08-07 20:52:53 +0300

    добавлен метод вычесления длины массива

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Метод создает массив из введеных пользователем эллементов с клавиатуры
+﻿// Метод создает массив из введеных пользователем элементов с клавиатуры
 // elements - кол-во элементов массива(задается пользователем с клавиатуры)
 string[] CreateArrayWords(int elements)
 {
@@ -6,28 +6,48 @@ string[] CreateArrayWords(int elements)
 
     for(int i = 0; i < elements; i++)
     {
-        Console.WriteLine($"Введите {i+1} эллемент массива: ");
+        Console.WriteLine($"Введите {i+1} элемент массива: ");
         string? n = Console.ReadLine();
         if(n != null)
             words[i] = n;
     }
     return words;
 }
-
-string[] FormingArrayStrings(string[] words)
+//Метод принимает на вход массив и считает кол-во искомых элементов меньше или равных 3
+// words - входящий массив
+int ElementCount(string[] words)
 {
-    string[] wordArray = new string[words.Length];
+    int count = 0;
+    for(int i = 0; i < words.Length; i++)
+        if(words[i].Length <= 3) count++;
+    
+    return count;
+}
+//Метод формирует новый массив из элементов входящего массива согласно условиям задачи
+// words - входящий массив
+// count - длина нового массива
+string[] FormingArrayStrings(string[] words, int count)
+{
+    string[] wordArray = new string[count];
+    int j = 0;
     for(int i = 0; i < words.Length; i++)
     {
-        if(words[i].Length <= 3)wordArray[i] = words[i];
+            if(words[i].Length <= 3)
+            {
+                wordArray[j] = words[i];
+                j++;
+            }
     }
     return wordArray;
 }
+
 Console.WriteLine("Введите кол-во элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 string[] mywords = CreateArrayWords(n);
-Console.WriteLine("[" + string.Join(" ", mywords) + "]");
+Console.WriteLine("Первоначальный массив: [" + string.Join(", ", mywords) + "]");
 
-string[] collectedWord = FormingArrayStrings(mywords);
-Console.WriteLine("[" + string.Join(" ",collectedWord) + "]");
+string[] collectedWord = FormingArrayStrings(mywords,ElementCount(mywords));
+Console.Write("Сформированный массив из строк длинна которых меньше либо равна 3 символа: ");
+Console.WriteLine("[" + string.Join(", ",collectedWord) + "]");
+
